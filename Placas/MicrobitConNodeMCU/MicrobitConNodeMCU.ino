7dfec5448dced0b329fbcf384a9407c418249111
Zeeno77 <glup87@gmail.com>
2022-09-19 14:07:35 -0300

    modified:   Placas/MicrobitConNodeMCU/MicrobitConNodeMCU.ino

--- Placas/MicrobitConNodeMCU/MicrobitConNodeMCU.ino
+++ Placas/MicrobitConNodeMCU/MicrobitConNodeMCU.ino
@@ -1,17 +1,17 @@
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>
-#include <SoftwareSerial.h> 
+#include <SoftwareSerial.h>
 #include <ESP8266HTTPClient.h>
 #include <Arduino_JSON.h>
 
 #define mySerialRx D6
 #define mySerialTx D7
 
-SoftwareSerial mySerial(mySerialRx, mySerialTx); 
+SoftwareSerial mySerial(mySerialRx, mySerialTx);
 
 //Configuracion del wifi
-const char* ssid = "arrakis";
-const char* password = "thespicemustflow";
+//const char* ssid = "arrakis";
+//const char* password = "thespicemustflow";
 //API REST URL
 const char* serverName = "http://192.168.88.53:8000/muestra/";
 
@@ -23,64 +23,112 @@ unsigned long timerDelay = 0;
 unsigned long lastTime = 0;
 String json = "";
 int httpResponseCode = 0;
-//String recivedData = "empty";
+String recivedData = "UNSET:NULL";
+String recivedCommand = "UNSET";
+String recivedValue = "";
+int separationIndex = -1;
 
 void setup() {
-Serial.begin(115200);
-mySerial.begin(9600);
-delay(10);
-WiFi.begin(ssid, password);
-while(WiFi.status() != WL_CONNECTED) {
-  delay(500);
-}
+  String ssid = "";
+  String password = "";
+  Serial.begin(115200);
+  mySerial.begin(9600);
+  delay(100);
+  //Get Wifi parameters via serial
+  Serial.println("Esperando Nombre de Red");
+  delay(100);
+  while ( recivedCommand != "NETWORK" ) {
+    while (!mySerial.available()) {
+    }
+    recivedData = mySerial.readString();
+    separationIndex = recivedData.indexOf(":");
+    if (separationIndex != -1) {
+      recivedCommand = recivedData.substring(0, separationIndex);
+      recivedValue = recivedData.substring(separationIndex + 1, recivedData.length());
+      Serial.println("recivedValue: " + recivedValue);
+      ssid = recivedValue;
+    }
+    Serial.println("recivedCommand: " + recivedCommand);
+    Serial.println("recibi: " + recivedData);
+    separationIndex = -1;
+  }
+  recivedData = "UNSET:NULL";
+  Serial.println("recibi: " + recivedData);
+  mySerial.print("OK_NETWORK");
+  Serial.println("Enviando OK_NETWORK");
+    
+  Serial.println("Esperando Password de Red");
+  while ( recivedCommand != "PASSWORD" ) {
+    while (!mySerial.available()) {
+    }
+    mySerial.print("OK_NETWORK");
+    Serial.println("Enviando OK_NETWORK");
+    recivedData = mySerial.readString();
+    separationIndex = recivedData.indexOf(":");
+    if (separationIndex != -1) {
+      recivedCommand = recivedData.substring(0, separationIndex);
+      recivedValue = recivedData.substring(separationIndex + 1, recivedData.length());
+      Serial.println("recivedValue: " + recivedValue);
+      password = recivedValue;
+    }
+    Serial.println("recivedCommand: " + recivedCommand);
+    Serial.println("recibi: " + recivedData);
+    separationIndex = -1;
+  }
+  mySerial.print("OK_PASSWORD");
+  //
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 
-Serial.println("");
+  Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
 }
- 
+
 void loop() {
-//Etapa 1 - Sensado
-    Serial.println("Esperando un mensaje...");
-    while (!mySerial.available()) {  
-    }
-    String recivedData = mySerial.readString();
-    Serial.println("recibi: "+ recivedData);
-  
-//if (isnan(recivedData) || isnan(t)) {
-//Serial.println("Error al obtener los datos");
-//return;
-//}
-// Abrir el Serial monitor para ver los datos
-//Etapa2 - Envio
+  //Etapa 1 - Sensado
+  Serial.println("Esperando un mensaje...");
+  while (!mySerial.available()) {
+  }
+  String recivedData = mySerial.readString();
+  Serial.println("recibi: " + recivedData);
+
+  //if (isnan(recivedData) || isnan(t)) {
+  //Serial.println("Error al obtener los datos");
+  //return;
+  //}
+  // Abrir el Serial monitor para ver los datos
+  //Etapa2 - Envio
   //Send an HTTP POST request every 30 seconds
 
 
-  
+
   if ((millis() - lastTime) > timerDelay) {
     //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
+    if (WiFi.status() == WL_CONNECTED) {
       WiFiClient client;
       HTTPClient http;
-      
+
       // Your Domain name with URL path or IP address with path
-      if (!http.begin(client,serverName)) {
+      if (!http.begin(client, serverName)) {
         Serial.println("connection failed");
         delay(5000);
         return;
-    }
-      
+      }
+
       http.addHeader("Content-Type", "application/json");
-      json = "{\"datosensado\":\"Data\",\"valor\":\""+recivedData+"\"}";
-      
+      json = "{\"datosensado\":\"Data\",\"valor\":\"" + recivedData + "\"}";
+
       Serial.println("Post:");
       Serial.println(json);
       httpResponseCode = http.POST(json);
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
-        
+
       // Free resources
       http.end();
     }
