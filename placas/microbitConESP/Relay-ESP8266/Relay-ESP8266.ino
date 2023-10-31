c468f7a43ee873f1ee96b93260bb2b5657ecf7fe
zeeno77 <31478274+zeeno77@users.noreply.github.com>
2023-10-31 01:31:01 -0300

    eliminadas impresiones inecesarias

--- placas/microbitConESP/Relay-ESP8266/Relay-ESP8266.ino
+++ placas/microbitConESP/Relay-ESP8266/Relay-ESP8266.ino
@@ -8,9 +8,10 @@
 #define mySerialTx D7
 
 SoftwareSerial mySerial(mySerialRx, mySerialTx);
+WiFiClient client;
+HTTPClient http;
 
 // Configuraciones Varias
-unsigned long sensorDelay = 5000;
 unsigned long timerDelay = 0;
 unsigned long lastTime = 0;
 int httpResponseCode = 0;
@@ -24,72 +25,46 @@ void setup()
   Serial.begin(115200);
   mySerial.begin(9600);
   delay(1000);
-  Serial.println("Iniciando Configuracion");
-  delay(1000);
   ssid = receiveCommand("NETWORK", "UNSET");
-  Serial.println("ssid: " + ssid);
   delay(1000);
   password = receiveCommand("PASSWORD", "NETWORK");
   delay(1000);
-  serverURL =  receiveCommand("SERVER", "PASSWORD");
+  serverURL = receiveCommand("SERVER", "PASSWORD");
   serverName = serverURL;
   delay(1000);
-  //
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
   }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop()
 {
-  // Etapa 1 - Sensado
-  Serial.println("Esperando un mensaje...");
-  while (!mySerial.available())
-  {
-  }
   String recivedData = receiveData();
-  Serial.println("recibi: " + recivedData);
-
   // Etapa2 - Envio
   // Send an HTTP POST request every timerDelay seconds
-
   if (((millis() - lastTime) > timerDelay) && (recivedData != "ERROR"))
   {
     // Check WiFi connection status
     if (WiFi.status() == WL_CONNECTED)
     {
-      WiFiClient client;
-      HTTPClient http;
-
       // Your Domain name with URL path or IP address with path
       if (!http.begin(client, serverName))
       {
-        Serial.println("connection failed");
         delay(5000);
         return;
       }
 
       http.addHeader("Content-Type", "application/json");
-      
-      Serial.println("Post:");
-      Serial.println(recivedData);
       httpResponseCode = http.POST(recivedData);
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
 
-      // Free resources
+      if (httpResponseCode > 0)
+      {
+        String response = http.getString();
+      }
+      // End the HTTP connection but keep the WiFiClient object alive
       http.end();
-    }
-    else
-    {
-      Serial.println("WiFi Disconnected");
     }
     lastTime = millis();
   }
@@ -101,35 +76,25 @@ String receiveCommand(String command, String previusCommand)
   String recivedValue = "";
   String recivedData = "UNSET:NULL";
   String recivedCommand = "UNSET";
-  Serial.println("Esperando " + command);
   int separationIndex = -1;
-  Serial.println("recibi: " + recivedData);
-  mySerial.print(okStr + previusCommand);
-  Serial.println("Enviando " + okStr + previusCommand);
   while (recivedCommand != command)
   {
     while (!mySerial.available())
     {
     }
     mySerial.print(okStr + previusCommand);
-    Serial.println("Enviando " + okStr + previusCommand);
     recivedData = mySerial.readString();
     separationIndex = recivedData.indexOf(":");
     if (separationIndex != -1)
     {
       recivedCommand = recivedData.substring(0, separationIndex);
       recivedValue = recivedData.substring(separationIndex + 1, recivedData.length());
-      Serial.println("recivedValue: " + recivedValue);
     }
-    Serial.println("recivedCommand: " + recivedCommand);
-    Serial.println("recibi: " + recivedData);
     separationIndex = -1;
   }
   mySerial.print(okStr + command);
-  Serial.println("Enviando " + okStr + command);
   return recivedValue;
 }
-
 
 String receiveData()
 {
@@ -139,7 +104,6 @@ String receiveData()
   String recivedAux = "";
   String recivedData = "";
   String json = "ERROR";
-  Serial.println("Esperando Datos");
   int separationIndex = -1;
   int secondSeparationIndex = -1;
   while (!mySerial.available())
@@ -154,9 +118,9 @@ String receiveData()
     secondSeparationIndex = recivedAux.indexOf(":");
     if (secondSeparationIndex != -1)
     {
-       recivedSensor =  recivedAux.substring(0, secondSeparationIndex);
-       recivedValue = recivedAux.substring(secondSeparationIndex + 1, recivedAux.length());
-             json = "{\"origen\":\"" + recivedOrigin + "\",\"sensor\":\"" + recivedSensor + "\",\"valor\":\"" + recivedValue + "\"}";
+      recivedSensor = recivedAux.substring(0, secondSeparationIndex);
+      recivedValue = recivedAux.substring(secondSeparationIndex + 1, recivedAux.length());
+      json = "{\"origen\":\"" + recivedOrigin + "\",\"sensor\":\"" + recivedSensor + "\",\"valor\":\"" + recivedValue + "\"}";
     }
   }
 
